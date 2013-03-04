57fc65e65211097efd93ca8ce5e735999e7fd56c
kpy <kpy00@web.de>
2013-03-04 19:50:06 +0100

    Add contact page to static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,7 +14,7 @@ describe "StaticPages" do
   end
 
   describe "Help page" do
-  	it "should have the content 'Help'" do
+  	it "should have the h1 'Help'" do
   		visit '/static_pages/help'
   		page.should have_selector('h1', :text => 'Help')
   	end
@@ -26,7 +26,7 @@ describe "StaticPages" do
   end
 
   describe "About page" do
-  	it "should have the content 'About Us'" do
+  	it "should have the h1 'About Us'" do
   		visit '/static_pages/about'
   		page.should have_selector('h1', :text => 'About Us')
   	end
@@ -35,5 +35,17 @@ describe "StaticPages" do
   		visit '/static_pages/about'
   		page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
   	end
+  end
+
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
 end
